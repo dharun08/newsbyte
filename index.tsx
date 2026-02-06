34e7531e0aac051de6133ef620a09bed28475abd
dharun08 <74301891+dharun08@users.noreply.github.com>
2026-02-06 16:20:03 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -7,7 +7,14 @@ const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
+.scrollbar-hide::-webkit-scrollbar {
+  display: none;
+}
 
+.scrollbar-hide {
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
