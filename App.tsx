c9b7adeade32ee2b880af130e668aa3146c19c69
dharun08 <74301891+dharun08@users.noreply.github.com>
2026-02-06 16:40:13 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -312,7 +312,7 @@ const App: React.FC = () => {
   return (
     <>
       <div className="flex flex-col h-screen font-sans bg-brand-darker">
-        <div className="w-full max-w-4xl mx-auto h-full flex flex-col bg-brand-dark/90 backdrop-blur-lg border border-bubble-border/30 shadow-2xl shadow-black/50 sm:rounded-xl my-0 sm:my-4 sm:h-[calc(100%-2rem)]">
+        <div className="w-full max-w-4xl mx-auto h-full flex flex-col bg-brand-dark/90 backdrop-blur-lg border border-bubble-border/30 shadow-2xl shadow-black/50 sm:rounded-xl my-0 sm:my-4 sm:h-[calc(100vh-2rem)]">
          <Header region={region} onRegionChange={setRegion} />
          <MarketSnapshot />  
           <TabNavigation activeTab={activeTab} onTabChange={setActiveTab} />
