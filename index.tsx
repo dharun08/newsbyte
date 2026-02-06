6a261c90ca4c4301b8b1dd0bc05f571375b85e4d
dharun08 <74301891+dharun08@users.noreply.github.com>
2026-02-06 16:54:10 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -7,14 +6,7 @@ const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
-.scrollbar-hide::-webkit-scrollbar {
-  display: none;
-}
 
-.scrollbar-hide {
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-}
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
