bcf517e9049696beb015611a3d7d79f4bac1bb2b
dharun08 <74301891+dharun08@users.noreply.github.com>
2026-02-06 13:50:33 +0530

    Update news.ts

--- api/news.ts
+++ api/news.ts
@@ -1,7 +1,5 @@
-export default async function handler(
-  req: VercelRequest,
-  res: VercelResponse
-) {
+export default async function handler(req: any, res: any)
+ {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
